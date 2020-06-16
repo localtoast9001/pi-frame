26ac225be62f6769f652bc857aa7e3c13b1e1cab
Jon Rowlett <motherplucker@gmail.com>
2020-06-16 15:45:12 -0700

    clean up files.

--- src/pi-frame-gtk/main.cpp
+++ src/pi-frame-gtk/main.cpp
@@ -1,63 +1,21 @@
-#include <gtkmm/application.h>
-#include <gtkmm/window.h>
+/**
+ * Copyright (C) Jon Rowlett. All rights reserved.
+ **/
+#include "main_window.hpp"
 
 using namespace Gtk;
 using namespace std;
 
-class main_window : public Window
-{
-public:
-  main_window(bool fullscreen = false)
-  : Window(), _fullscreen(fullscreen)
-  {
-    set_title("Pi Frame");
-    resize(200, 200);
-
-    add_events(Gdk::KEY_PRESS_MASK);
-    if (_fullscreen)
-    {
-      this->fullscreen();
-    }
-  }
-
-  virtual ~main_window()
-  {
-  }
-
-protected:
-  virtual bool on_key_press_event(GdkEventKey* event)
-  {
-    if (event->keyval == GDK_KEY_F11)
-    {
-      on_toggle_fullscreen();
-      return true;
-    }
-
-    return false;
-  }
-
-  virtual void on_toggle_fullscreen()
-  {
-    if (_fullscreen)
-    {
-      unfullscreen();
-    }
-    else
-    {
-      fullscreen();
-    }
-
-    _fullscreen = !_fullscreen;
-  }
-
-private:
-  bool _fullscreen;
-};
-
+/**
+ * Main method
+ * @param argc count of arguments.
+ * @param argv array of C string arguments.
+ * @returns 0 for success. non-zero for failure.
+ **/
 int main(int argc, char* argv[])
 {
   Glib::RefPtr<Application> app = Application::create(argc, argv, "org.example.pi-frame-gtk"); 
-  main_window window;
+  pi_frame::main_window window;
 
   return app->run(window);
 }
---
+++ src/pi-frame-gtk/main_window.cpp
@@ -0,0 +1,51 @@
+/**
+ * Copyright (C) Jon Rowlett. All rights reserved.
+ **/
+#include "main_window.hpp"
+
+using namespace Gtk;
+
+namespace pi_frame
+{
+    main_window::main_window(bool fullscreen)
+    : Window(), _fullscreen(fullscreen)
+    {
+        set_title("Pi Frame");
+        resize(200, 200);
+
+        add_events(Gdk::KEY_PRESS_MASK);
+        if (_fullscreen)
+        {
+            this->fullscreen();
+        }
+    }
+
+    main_window::~main_window()
+    {
+    }
+
+    bool main_window::on_key_press_event(GdkEventKey* event)
+    {
+        if (event->keyval == GDK_KEY_F11)
+        {
+            toggle_fullscreen();
+            return true;
+        }
+
+        return false;
+    }
+
+    void main_window::toggle_fullscreen()
+    {
+        if (_fullscreen)
+        {
+            unfullscreen();
+        }
+        else
+        {
+            fullscreen();
+        }
+
+        _fullscreen = !_fullscreen;
+    }
+} // namespace pi_frame
